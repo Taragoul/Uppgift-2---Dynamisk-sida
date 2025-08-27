046eb3e264900fe38cbcfa43fe48d9e7f1fb37c3
Zak <zaktroy1@gmail.com>
2025-08-27 08:58:20 +0200

    rendered the CV on its page with some styling

--- src/pages/MyCV.jsx
+++ src/pages/MyCV.jsx
@@ -1,3 +1,18 @@
 import React from "react";
-const MyCV = () => <div>My CV Page</div>;
+import cvData from "../assets/cv.json";
+import "../assets/styles/MyCV.css";
+
+const MyCV = () => (
+  <div className="cv-container">
+    <h2>My CV</h2>
+    {cvData.map((item, idx) => (
+      <div className="cv-entry" key={idx}>
+        <h3>{item.name}</h3>
+        <div className="cv-time">{item.time}</div>
+        <div>{item.description}</div>
+      </div>
+    ))}
+  </div>
+);
+
 export default MyCV;
